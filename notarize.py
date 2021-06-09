722cee842d04cee80226696ef204f933d8019f86
davecotter <dave@kjams.com>
2021-06-09 12:37:55 -0700

    updt for bigsur, ignore non-error, code factoring

--- notarize.py
+++ notarize.py
@@ -6,83 +6,153 @@ import sys
 import time
 
 def log_message(message, newline=True):
-    sys.stderr.write(message)
-    if newline:
-        sys.stderr.write("\n")
+	if newline:
+		message += '\n'
+		
+	sys.stderr.write(message)
 
+# commented out lines help with debugging
+# now matches multiline strings
+def regmatch(keyStr, inStr):
+#	log_message('trying to find "' + keyStr + '" inside "' + inStr + '"')
+	m = re.search(keyStr, inStr, re.M)
+# 	if m:
+# 		log_message("found!")
+# 	else:
+# 		log_message("nope")
 
-def upload_package(args):
-    process = subprocess.Popen(['xcrun', 'altool', '--notarize-app', '-t', 'osx', '-f', args.package,
-                                '--primary-bundle-id', args.primary_bundle_id, '-u', args.username,
-                                '--output-format', 'json', '-p', args.password],
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    log_message('>> Uploading dmg to apple')
-    output, error = process.communicate()
-    output_str = output.decode('utf-8')
-    error_str = error.decode('utf-8')
-    log_message(output_str)
-    log_message(error_str)
-    if not 'No errors uploading' in error_str:
-        log_message('[Error] Upload failed')
-        exit(1)
-    m = re.match('.*RequestUUID = (.*)\n', error_str, re.S)
-    if not m:
-        log_message('[Error] No UUID created')
-        exit(1)
-    uuid = m.group(1)
-    log_message('>> Job UUID: %s' % uuid)
-    return uuid
+	return m
 
+# supports three styles of matching: 
+# space equals space (... = ...\n)
+# colon space (...: ...\n)
+# json style (...":"...")
+def regex_getVal(inStr, keyStr, throwB = True):
+	valStr = ''
+	
+	m = regmatch('.*' + keyStr + ' = (.*)$', inStr)
+	if not m: m = regmatch('.*' + keyStr + ': (.*)$', inStr)
+	if not m: m = regmatch('.*"' + keyStr + '":"(.*?)"', inStr)
+	if not m:
+		log_message('[Error] "' + keyStr + '" key not found')
+		
+		if throwB:
+			exit(1)
+	else:
+		valStr = m.group(1)
 
-def check_status(args, uuid):
-    process = subprocess.Popen(['xcrun', 'altool', '--notarization-info', uuid,
-                                '-u', args.username, '-p', args.password, '--output-format', 'json'],
-                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    output_str = output.decode('utf-8')
-    error_str = error.decode('utf-8')
+	# log_message('>> "' + keyStr + '": "' + valStr + '"')
+	return valStr
 
-    in_progress = 'Status: in progress' in error_str
-    success = 'Status: success' in error_str
+def pipe(cmdArgs):
+	process = subprocess.Popen(cmdArgs, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	output, error = process.communicate()
+	output_str = output.decode('utf-8')
+	error_str = error.decode('utf-8')
 
-    if not in_progress and not success:
-        log_message(output_str)
-        log_message(error.decode('utf-8'))
-        log_message('[Error] Notarization failed')
-        exit(1)
+	# skip over this noisy NON-error. if we get it, then move the real results
+	# into the error_str, which is the one we want returned anyway
+	if 'AcceptPolicy_block_invoke' in error_str:
+		error_str = output_str
 
-    return in_progress
+# 	log_message("out:" + output_str)
+# 	log_message("err: " + error_str)
+	return error_str
 
+def upload_package(dict):
 
-def staple(args):
-    process = subprocess.Popen(['xcrun', 'stapler', 'staple', args.package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    log_message(output.decode('utf-8'))
-    log_message(error.decode('utf-8'))
+	log_message('>> Notarizing: Uploading dmg to Apple')
 
+	error_str = pipe(
+		['xcrun', 
+			'altool', 
+			'--notarize-app', 
+			'-t', 'osx', 
+			'--primary-bundle-id',	dict['primary_bundle_id'], 
+			'-f',					dict['package'],
+			'-u',					dict['username'],
+			'-p',					dict['password'],
+			'--output-format', 		'json'])
 
-def main():
-    parser = argparse.ArgumentParser(description="Notarizes supplied dmg by uploading it to apple servers.")
-    parser.add_argument("--package", help="Path to the dmg file", action='store', required=True)
-    parser.add_argument("--username", help="Apple ID username to use to notarize", action='store', required=True)
-    parser.add_argument("--primary-bundle-id", help="Bundle id of package as specified in Info.plist", action='store', required=True)
-    parser.add_argument("--password", action='store', help="Password for the appleid.", required=True)
-    args = parser.parse_args()
+	if not 'No errors uploading' in error_str:
+		log_message('[Error] Upload failed')
+		exit(1)
+		
+	uuid = regex_getVal(error_str, 'RequestUUID') 
+	return uuid
 
-    if not args.package.endswith('.dmg'):
-        log_message('Supplied package %s is not a dmg file' % args.package)
-        exit(1)
+def check_status(dict):
+	#log_message('>> Checking status...')
+	
+	error_str = pipe(
+		['xcrun', 
+			'altool', 
+			'--notarization-info',	dict['uuid'],
+			'-u',					dict['username'], 
+			'-p',					dict['password'], 
+			'--output-format',		'json'])
 
-    uuid = upload_package(args)
+	statusStr = regex_getVal(error_str, 'Status') 
+	
+	in_progressB	= statusStr == 'in progress'
+	successB		= statusStr == 'success'
 
-    while check_status(args, uuid):
-        log_message('.. Notarization in progress. Checking back in 30s')
-        time.sleep(30)
-    log_message('>> Notarization successful')
-    log_message('>> Stapling')
-    staple(args)
-    log_message('[Success] All done')
+	if not in_progressB and not successB:
+		log_message(error_str)
+		log_message('[Error] Notarization failed')
+		exit(1)
 
+	if not in_progressB:
+		log_message('\t>> Notarization successful')
 
+	return in_progressB
+
+def staple(dict):
+	packageStr = dict['package']
+
+	log_message('>> Stapling')
+	error_str = pipe(
+		['xcrun', 
+			'stapler', 
+			'staple', packageStr])
+
+# you can now call this func from another python script, passing in 
+# a dict-style argument block instead of actual args.  
+# note the key for --primary-bundle-id is actually 'primary_bundle_id'
+def call(dict):
+	#log_message('dict: ' + str(dict))
+	packageStr = dict['package']
+	
+	if not packageStr.endswith('.dmg'):
+		log_message('Supplied package %s is not a dmg file' % packageStr)
+		exit(1)
+
+	uuid = upload_package(dict)
+	
+	# add uuid to "args" dict
+	dict['uuid'] = uuid
+
+	# do-while here, instead of while-do, otherwise first check will fail 
+	while True:
+		log_message('\t>> Notarization in progress. Checking back in 30s')
+		time.sleep(30)
+		if not check_status(dict):
+			break
+		
+ 	staple(dict)
+
+	log_message('[Success] Notarization complete')
+	
+# if you want to call it from another script, with args, you still can
+def main(in_args):
+	parser = argparse.ArgumentParser(description="Notarizes supplied dmg by uploading it to apple servers.")
+	parser.add_argument("--package", help="Path to the dmg file", action='store', required=True)
+	parser.add_argument("--username", help="Apple ID username to use to notarize", action='store', required=True)
+	parser.add_argument("--primary-bundle-id", help="Bundle id of package as specified in Info.plist", action='store', required=True)
+	parser.add_argument("--password", action='store', help="Password for the appleid.", required=True)
+	args = parser.parse_args(in_args)
+	call(vars(args))
+
+# and can still call from command line, of course
 if __name__ == '__main__':
-    main()
+	main(sys.argv[1:])
